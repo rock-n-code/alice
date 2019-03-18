47c917476714f3900d5a6aef0df6cd49d9083fc2
Javier Cicchelli <javier@rock-n-code.com>
2019-03-18 17:58:57 +0100

    Moved mocks to their respective "Mock" folders.

--- Tests/AliceTests/XCTestManifests.swift
+++ Tests/AliceTests/XCTestManifests.swift
@@ -3,7 +3,6 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [testCase(IdentifiableTests.allTests),
-			testCase(IntExtensionsTests.allTests),
 			testCase(ScriptTests.allTests),
 			testCase(ScriptableTests.allTests),]
 }
