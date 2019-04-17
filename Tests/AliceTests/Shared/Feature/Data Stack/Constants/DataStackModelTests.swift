f5cd0b3a24ae1370d5d9b2aeb786cd45fa24c392
Javier Cicchelli <javier@rock-n-code.com>
2019-04-17 16:57:17 +0200

    Created and implemented DataStackModel.

---
+++ Tests/AliceTests/Shared/Feature/Data Stack/Constants/DataStackModelTests.swift
@@ -0,0 +1,47 @@
+//
+//  DataStackModelTests.swift
+//  Alice
+//
+//  Created by Javier Cicchelli on 17/04/2019.
+//
+
+import XCTest
+
+@testable import Alice
+
+class DataStackModelTests: XCTestCase {
+
+	// MARK: Static
+	
+	static var allTests = [
+		("testHashValue", testHashValue),
+		("testEquality", testEquality),
+	]
+	
+	// MARK: Setup
+	
+    override func setUp() {
+		super.setUp()
+    }
+
+    override func tearDown() {
+		super.tearDown()
+    }
+	
+	// MARK: Properties tests
+	
+	func testHashValue() {
+		XCTAssertGreaterThanOrEqual(DataStack.Model.test.hashValue, Int.min)
+		XCTAssertLessThanOrEqual(DataStack.Model.test.hashValue, Int.max)
+		XCTAssertGreaterThanOrEqual(DataStack.Model.unknown.hashValue, Int.min)
+		XCTAssertLessThanOrEqual(DataStack.Model.unknown.hashValue, Int.max)
+	}
+	
+	// MARK: Functions tests
+
+    func testEquality() {
+		XCTAssertEqual(DataStack.Model.test, DataStack.Model("Test"))
+		XCTAssertEqual(DataStack.Model.unknown, DataStack.Model("Something"))
+    }
+
+}
---
+++ Tests/AliceTests/Shared/Mock/Constants/DataStackModelMock.swift
@@ -0,0 +1,13 @@
+//
+//  DataStackModelMock.swift
+//  Alice
+//
+//  Created by Javier Cicchelli on 17/04/2019.
+//
+
+import Alice
+
+extension DataStack.Model {
+	internal static let test = DataStack.Model("Test")
+	internal static let unknown = DataStack.Model("Something")
+}
