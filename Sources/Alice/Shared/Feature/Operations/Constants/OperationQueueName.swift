33867392cfb7f07d65d14d5b2cd90979f0fc8485
Javier Cicchelli <javier@rock-n-code.com>
2019-04-19 12:26:29 +0200

    Created and defined OperationQueueName.

---
+++ Sources/Alice/Shared/Feature/Operations/Constants/OperationQueueName.swift
@@ -0,0 +1,17 @@
+//
+//  OperationQueueName.swift
+//  Alice
+//
+//  Created by Javier Cicchelli on 19/04/2019.
+//
+
+import Foundation
+
+extension OperationQueue {
+	
+	// MARK: Constants
+	
+	/// Container for the name of the operation queue.
+	public class Name: Value<String> {}
+	
+}
