416365ccf06987152aca3c66accf545a98badcce
Javier Cicchelli <javier@rock-n-code.com>
2019-04-20 20:51:43 +0200

    Created and implemented Pausable and Resumeable.

---
+++ Sources/Alice/Shared/Common/Protocols/Pauseable.swift
@@ -0,0 +1,16 @@
+//
+//  Pauseable.swift
+//  Alice
+//
+//  Created by Javier Cicchelli on 20/04/2019.
+//
+
+/// Protocol to implement the "Pause" functionality.
+public protocol Pauseable {
+	
+	// MARK: Functions
+	
+	/// Pauses an action.
+	func pause()
+	
+}
---
+++ Tests/AliceTests/Shared/Common/Protocols/PausableTests.swift
@@ -0,0 +1,42 @@
+//
+//  PausableTests.swift
+//  Alice
+//
+//  Created by Javier Cicchelli on 20/04/2019.
+//
+
+import XCTest
+
+@testable import Alice
+
+class PausableTests: XCTestCase {
+
+	// MARK: Static
+	
+	static var allTests = [
+		("testPause", testPause),
+	]
+	
+	// MARK: Setup
+	
+    override func setUp() {
+		super.setUp()
+    }
+
+    override func tearDown() {
+		super.tearDown()
+    }
+	
+	// MARK: Functions tests
+	
+	func testPause() {
+		let mock = PausableMock()
+		
+		XCTAssertFalse(mock.isPaused)
+		
+		mock.pause()
+		
+		XCTAssertTrue(mock.isPaused)
+	}
+
+}
