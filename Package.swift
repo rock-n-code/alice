038bf35cd57b2bccb1f5abb766f75f436573717d
Javier Cicchelli <javier@rock-n-code.com>
2019-05-17 09:17:30 +0200

    Defined platforms in Package file.

--- Package.swift
+++ Package.swift
@@ -4,8 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "Alice",
+	platforms: [
+		.macOS(.v10_14),
+		.iOS(.v12),
+		.tvOS(.v12),
+		.watchOS(.v5)
+	],
     products: [
         .library(name: "Alice",
+				 type: .dynamic,
 				 targets: ["Alice"]),
     ],
     dependencies: [],
